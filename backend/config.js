81d53ea6848bc30a95f48c967d566da0b4f6a494
harsha1464 <136953219+harsha1464@users.noreply.github.com>
2024-06-11 15:20:42 +0530

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
 export const PORT = 5555;
 
 export const mongoDBURL =
-  'mongodb+srv://admin:admin@backenddb.37xlggb.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB';
+  'mongodb+srv://admin:<password>@backenddb.37xlggb.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB';
 
